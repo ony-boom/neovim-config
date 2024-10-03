37def68035892f367dd7d19258ed3296ce190d1a
Ony <onyrakoto27@gmail.com>
2024-10-03 15:26:39 +0300

    fix theme

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,7 +1,6 @@
 return {
   "sainnhe/everforest",
   config = function ()
-      vim.g.everforest_better_performance = 1
       vim.g.everforest_transparent_background = 1
 
       local winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:TelescopeSelection,Search:None"
