8f29f0afc49f3ed9282d09b45937dfb0304c9504
Ony <onyrakoto27@gmail.com>
2024-10-03 11:27:25 +0300

    init

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+return {
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    enabled = false,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    enabled = false,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    enabled = false,
+  },
+}
