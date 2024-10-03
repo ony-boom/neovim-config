42e563ccda29baefe2a58186869530f43154ee62
Ony <onyrakoto27@gmail.com>
2024-10-03 15:28:56 +0300

    totally transparent

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,7 +1,7 @@
 return {
   "sainnhe/everforest",
   config = function ()
-      vim.g.everforest_transparent_background = 1
+      vim.g.everforest_transparent_background = 2
 
       local winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:TelescopeSelection,Search:None"
       require("cmp").setup {
