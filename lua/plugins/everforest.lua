d39451f4edcf1ff634b8c72ed62600abb3d9197b
Ony <onyrakoto27@gmail.com>
2024-10-03 15:25:29 +0300

    slight transparency of neovim

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -2,7 +2,7 @@ return {
   "sainnhe/everforest",
   config = function ()
       vim.g.everforest_better_performance = 1
-      vim.g.everforest_transparent_background = 2
+      vim.g.everforest_transparent_background = 1
 
       local winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:TelescopeSelection,Search:None"
       require("cmp").setup {
