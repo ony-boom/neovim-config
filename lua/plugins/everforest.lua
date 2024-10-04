2e39de58c7ed91176a77c45979572815f56b5bbb
Ony <onyrakoto27@gmail.com>
2024-10-04 10:37:06 +0300

    better neotree ui

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,18 +1,23 @@
 return {
   "sainnhe/everforest",
-  config = function ()
-      vim.g.everforest_transparent_background = 2
+  config = function()
+    vim.g.everforest_transparent_background = 2
 
-      local winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:TelescopeSelection,Search:None"
-      require("cmp").setup {
-        window = {
-          completion = {
-            winhighlight = winhighlight,
-          },
-          documentation = {
-            winhighlight = winhighlight,
-          },
+    local winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:TelescopeSelection,Search:None"
+    require("cmp").setup {
+      window = {
+        completion = {
+          winhighlight = winhighlight,
         },
-      }
-  end
+        documentation = {
+          winhighlight = winhighlight,
+        },
+      },
+    }
+
+    require("neo-tree").setup {
+      highlight_tap = "WhiteSpace",
+      highlight_background = "None",
+    }
+  end,
 }
