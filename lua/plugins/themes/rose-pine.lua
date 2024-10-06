683b61013b47bde78498bcbe44528b488aaf0b5d
Ony <onyrakoto27@gmail.com>
2024-10-06 20:14:01 +0300

    update colorscheme

---
+++ lua/plugins/themes/rose-pine.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup {
+      styles = {
+        transparency = true,
+      },
+    }
+  end,
+}
