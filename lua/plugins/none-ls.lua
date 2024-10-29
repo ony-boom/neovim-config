1de1226f1f0d6c083f310a326da491ec61d9194c
Ony <onyrakoto27@gmail.com>
2024-10-29 12:03:19 +0300

    none-ls fix

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,60 +1,72 @@
+local mkHandler = function(type, client, conditionFn)
+  local null_ls = require "null-ls"
+  null_ls.register(null_ls.builtins[type][client].with {
+    condition = conditionFn,
+  })
+end
 -- Customize None-ls sources
 ---@type LazySpec
 return {
-  "nvimtools/none-ls.nvim",
-  opts = function(_, opts)
-    -- opts variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      -- opts variable is the default configuration table for the setup function call
+      local null_ls = require "null-ls"
 
-    -- Check supported formatters and linters
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+      -- Check supported formatters and linters
+      -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+      -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 
-    -- Only insert new sources, do not replace the existing ones
-    -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
-    --
+      -- Only insert new sources, do not replace the existing ones
+      -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
+      --
 
-    local deno_fmt = {
-      method = null_ls.methods.FORMATTING,
-      filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "json", "jsonc" },
-      generator = null_ls.formatter {
-        command = "deno",
-        args = { "fmt", "--stdin" },
-        to_stdin = true,
-      },
-    }
-    opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Set a formatter
-      null_ls.builtins.formatting.shfmt,
-      null_ls.builtins.formatting.gofmt,
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.prettierd,
-      null_ls.builtins.formatting.alejandra,
-      deno_fmt,
-      -- null_ls.builtins.formatting.deno_fmt,
-    })
-
-    local mkHandler = function(type, client, conditionFn)
-      null_ls.register(null_ls.builtins[type][client].with {
-        condition = conditionFn,
+      local deno_fmt = {
+        method = null_ls.methods.FORMATTING,
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "json", "jsonc" },
+        generator = null_ls.formatter {
+          name = "deno_fmt",
+          command = "deno",
+          args = { "fmt", "$FILENAME", "--stdin" },
+          to_stdin = true,
+        },
+      }
+      opts.sources = require("astrocore").list_insert_unique(opts.sources, {
+        -- Set a formatter
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.alejandra,
+        deno_fmt,
+        -- null_ls.builtins.formatting.deno_fmt,
       })
-    end
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = {
+      handlers = {
+        prettierd = mkHandler("formatting", "prettierd", function(utils)
+          local prettierdConfigFiles = {
+            ".prettierrc",
+            ".prettierrc.js",
+            ".prettierrc.json",
+            "prettier.config.js",
+          }
 
-    opts.handlers = {
-      prettierd = mkHandler("formatting", "prettierd", function(utils)
-        local prettierdConfigFiles = {
-          ".prettierrc",
-          ".prettierrc.js",
-          ".prettierrc.json",
-          "prettier.config.js",
-        }
+          for _, value in pairs(prettierdConfigFiles) do
+            if utils.root_has_file(value) then return true end
+          end
 
-        for _, value in pairs(prettierdConfigFiles) do
-          if utils.root_has_file(value) then return true end
-        end
-
-        return utils.root_has_file "package.json"
-      end),
-    }
-  end,
+          return utils.root_has_file "package.json"
+        end),
+      },
+      deno_fmt = mkHandler(
+        "formatting",
+        "deno_fmt",
+        function(utils) return utils.root_has_file "deno.json" or utils.root_has_file "deno.jsonc" end
+      ),
+    },
+  },
 }
