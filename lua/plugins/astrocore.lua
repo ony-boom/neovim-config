0b44e232b68a553a87ab3475aaa1f5ba385a4081
Ony <onyrakoto27@gmail.com>
2024-11-18 06:31:13 +0300

    append path to vim path

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -25,6 +25,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        path = vim.opt.path + "**",
       },
       g = {},
     },
