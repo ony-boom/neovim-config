cca5d5ce87b8142e8c50c27def08cf84401cb554
Ony <onyrakoto27@gmail.com>
2025-03-10 22:10:47 +0300

    using light theme

--- lua/plugins/themes/rose-pine.lua
+++ lua/plugins/themes/rose-pine.lua
@@ -1,19 +1,19 @@
 ---@type LazySpec
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup {
-      variant = "main",
-      dark_variant = "main",
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        require("rose-pine").setup {
+            variant = "dawn",
+            dark_variant = "main",
 
-      styles = {
-        transparency = true,
-      },
+            styles = {
+                transparency = true,
+            },
 
-      highlight_groups = {
-        NotifyBackground = { bg = "base", fg = "text" },
-      },
-    }
-  end,
+            highlight_groups = {
+                NotifyBackground = { bg = "base", fg = "text" },
+            },
+        }
+    end,
 }
