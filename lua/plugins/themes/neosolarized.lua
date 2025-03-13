b5fdbafb1a2c1017797359957377bcf39342f404
Ony <onyrakoto27@gmail.com>
2025-03-13 10:02:42 +0300

    upadted neovim config colors

--- lua/plugins/themes/neosolarized.lua
+++ lua/plugins/themes/neosolarized.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: inject-field, undefined-field
 return {
   "craftzdog/solarized-osaka.nvim",
   lazy = false,
@@ -7,6 +8,42 @@ return {
     require("solarized-osaka").setup {
       transparent = true, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+
+      styles = {
+        sidebars = "transparent",
+        float = "transparent",
+      },
+
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
     }
   end,
 }
