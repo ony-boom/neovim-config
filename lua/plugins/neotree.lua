7f8ac29e814b287d1ae4467cfee2a5646e10e82e
Ony <onyrakoto27@gmail.com>
2025-04-08 20:02:31 +0300

    filesystem only start

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,4 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = function(_, opts) opts.sources = { "filesystem" } end,
+  opts = function(_, opts)
+    opts.sources = { "filesystem" }
+    opts.source_selector.content_layout = "start"
+  end,
 }
