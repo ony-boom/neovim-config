64aab5b950e7442d9e40efb920d92bb1328a6674
Ony <onyrakoto27@gmail.com>
2025-04-15 15:22:47 +0300

    nvim conf update
    remove some useless code

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,6 @@
 ---@type LazySpec
 return {
   {
-    "williamboman/mason-lspconfig.nvim",
-    enabled = false,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    enabled = false,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    enabled = false,
-  },
-  {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     enabled = false,
     opts = {
