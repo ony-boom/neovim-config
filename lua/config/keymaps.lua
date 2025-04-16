60a4cc5ae6378cf2111c36d2ce91f11f412d05b5
Ony <onyrakoto27@gmail.com>
2025-04-16 14:38:13 +0300

    add lualine

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,17 @@
 local map = function(mode, lhs, rhs, opts)
-	local options = { silent = true, noremap = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.keymap.set(mode, lhs, rhs, options)
+    local options = { silent = true, noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
 end
 
 map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
 map({ "n", "i" }, "<C-s>", ":w<CR>", { desc = "Save File" })
+map("n", "<leader>c", ":bd<CR>", { desc = "Close buffer" })
+map("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
+map("n", "[b", ":bprevious<CR>", { desc = "Previous buffer" })
 
 return {
-	map = map,
+    map = map,
 }
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+
+	config = function()
+		require("lualine").setup({
+			options = {
+				component_separators = "",
+				section_separators = { right = "", left = "" },
+			},
+
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "filetype" },
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+		})
+	end,
+}
