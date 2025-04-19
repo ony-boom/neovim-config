f40ef1c125b980cd0f9c1f647e6fb3474a9359b0
Ony <onyrakoto27@gmail.com>
2025-04-19 12:43:26 +0300

    enable highlight in treesiter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
 
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html" },
-		})
-	end,
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      sync_install = false,
+
+      modules = {},
+
+      auto_install = true,
+      ignore_install = {},
+      ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html" },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    }
+  end,
 }
