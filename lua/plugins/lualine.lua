749b5d43df3fa41c4244b6f46f23ad8247b0bf9a
Ony <onyrakoto27@gmail.com>
2025-04-19 19:13:08 +0300

    lualine simplify

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,21 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+  "nvim-lualine/lualine.nvim",
 
-	config = function()
-		require("lualine").setup({
-			options = {
-				component_separators = "",
-				section_separators = { right = "", left = "" },
-			},
+  config = function()
+    require("lualine").setup {
+      options = {
+        component_separators = "",
+        section_separators = { right = "", left = "" },
+      },
 
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "filetype" },
-				lualine_y = {"lsp_status"},
-				lualine_z = { "location" },
-			},
-		})
-	end,
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "filetype" },
+        lualine_y = { "lsp_status" },
+        lualine_z = { "location" },
+      },
+    }
+  end,
 }
